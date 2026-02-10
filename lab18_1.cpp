566f7b65f9567b2f5335bcca66557ad995d3a318
D <phuuuumi@gmail.com>
2026-02-10 20:45:52 +0700

    lab18_1 and lab18_2 committed

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -1,12 +1,40 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
 struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect a, Rect b){
+	if(a.y > b.y){
+		if(a.y - a.h >= b.h)return 0;
+		if(a.x > b.x){
+			if(b.x + b.w <= a.x) return 0;
+			double x = b.x + b.w - a.x, y = b.y - (a.y - a.h);
+			return x * y;
+		}else{
+			if(a.x + a.w <= b.x) return 0;
+			double x = min(b.w, a.x + a.w - b.x), y = min(b.h, b.y - (a.y - a.h));
+			return x * y;
+		}
+	}else{
+		if(b.y - b.h >= a.h)return 0;
+		if(b.x > a.x){
+			if(a.x + a.w <= b.x) return 0;
+			double x = a.x + a.w - b.x, y = a.y - (b.y - b.h);
+			return x * y;
+		}else{
+			if(b.x + b.w <= a.x) return 0;
+			double x = min(a.w, b.x + b.w - a.x), y = min(a.h, a.y - (b.y - b.h));
+			return x * y;
+		}
+	}
+}
 
 
-
+int main(){
+	Rect R1 = {-1,2,6.9,9.6};
+	Rect R2 = {0,0,1.2,2.5};
+	cout << overlap(R1,R2);
 }
--- lab18_2.cpp
+++ lab18_2.cpp
@@ -5,8 +5,35 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect* a, Rect* b){
+	if(a->y > b->y){
+		if(a->y - a->h >= b->h)return 0;
+		if(a->x > b->x){
+			if(b->x + b->w <= a->x) return 0;
+			double x = b->x + b->w - a->x, y = b->y - (a->y - a->h);
+			return x * y;
+		}else{
+			if(a->x + a->w <= b->x) return 0;
+			double x = min(b->w, a->x + a->w - b->x), y = min(b->h, b->y - (a->y - a->h));
+			return x * y;
+		}
+	}else{
+		if(b->y - b->h >= a->h)return 0;
+		if(b->x > a->x){
+			if(a->x + a->w <= b->x) return 0;
+			double x = a->x + a->w - b->x, y = a->y - (b->y - b->h);
+			return x * y;
+		}else{
+			if(b->x + b->w <= a->x) return 0;
+			double x = min(a->w, b->x + b->w - a->x), y = min(a->h, a->y - (b->y - b->h));
+			return x * y;
+		}
+	}
+}
 
 
-
+int main(){
+	Rect R1 = {1,1,5,5};
+	Rect R2 = {7,2,3,3};	
+	cout << overlap(&R1,&R2);
 }
